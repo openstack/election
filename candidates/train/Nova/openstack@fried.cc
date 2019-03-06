76ef4b77aca8e4a284dbcff3a13b18489b0c385c
Eric Fried <openstack@fried.cc>
2019-03-05 18:43:22 -0600

    Add Eric Fried candidacy for Nova PTL
    Change-Id: I0a143671a024a85fe53e0ea8acbc6b8d832688fe

---
+++ candidates/train/Nova/openstack@fried.cc
@@ -0,0 +1,52 @@
+Greetings. I am hereby announcing my candidacy for Nova PTL for Train.
+
+I am passionate about OpenStack, to the point where I (like many of you)
+changed companies so I wouldn't have to change jobs. My employer backs
+me as a 100% upstream developer and core in Nova and Placement, and
+fully supports my commitment to the role of Nova PTL if you'll have me.
+
+Nova is huge, in code as well as in history. I don't pretend to
+understand all (or even most) of it. It takes the cumulative skill of
+the whole team to make the project tick. I see the PTL as a facilitator
+and coordinator of that "group brain", making the whole greater than the
+sum of the parts. That takes organization, diplomacy, and patience:
+qualities I have demonstrated by running the nova-scheduler meetings,
+helping with the politics of extracting Placement, and engaging in tough
+and contentious design discussions in specs, PTGs, IRC, etc. As PTL, one
+of my main goals will be to keep those conversations moving and focused,
+avoiding unproductive tangents.
+
+We continue to face challenges in contributor (which includes reviewer!)
+participation. Partly this is due to the overall change in the character
+of the community; but I hope to break down barriers for new or part-time
+contributors who are willing and able to participate. In day-to-day
+operations this means being responsive and encouraging on IRC and
+gerrit. In forum and PTG sessions it means working to avoid the "echo
+chamber effect" and ceding the floor to these less-involved participants
+so they can be heard. I would also like to continue blueprint tracking
+including runways, which has been an effective way to ensure attention
+for efforts outside of our deemed main themes and priorities.
+
+Nova is on the cusp of being able to take advantage of advanced
+Placement features such as nested and sharing providers to solve some of
+the problems for which Placement was originally conceived. Nova will
+need to continue to work closely with Placement in its new position as
+an independent project. I intend to continue being an active Placement
+core regardless of the outcome of these elections to assist in that
+transition.
+
+We need to continue to foster our relationships with other teams,
+ensuring proper care and feeding of cross-project initiatives. This
+applies to the old guard (Cinder, Glance, Ironic, Keystone, Neutron, and
+Placement as noted) but also to nascent efforts such as Cyborg. This
+starts with forum and PTG logistics for Denver, and continues with
+constant communication during the cycle.
+
+I am aware that I am standing on the shoulders of giants. I'm not trying
+to do anything brilliantly new here. I want to follow the lead of my
+predecessors and continue to tap them for their expertise and experience
+to help Nova succeed in Train.
+
+Thank you for your consideration.
+Eric Fried (efried)
+(rhymes with 'deed', not with 'side')
