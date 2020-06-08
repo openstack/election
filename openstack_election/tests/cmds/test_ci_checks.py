c08aed239d68d925f4bcd57457f68d0631c35f40
Hervé Beraud <hberaud@redhat.com>
2020-06-09 00:09:14 +0200

    Use unittest.mock instead of mock
    The mock third party library was needed for mock support in py2
    runtimes. Since we now only support py36 and later, we can use the
    standard lib unittest.mock module instead.
    
    Change-Id: I0b5e9e60b00204f938d23fc4ff8e020ebb4afa43

--- openstack_election/tests/cmds/test_ci_checks.py
+++ openstack_election/tests/cmds/test_ci_checks.py
@@ -10,7 +10,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import mock
+from unittest import mock
 
 from openstack_election.cmds import ci_check_all_candidate_files
 from openstack_election.tests import base
