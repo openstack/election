3676e94728565480b58d7c2546f73fc272d086d9
Jay Faulkner <jay@jvf.cc>
2022-09-06 14:05:27 -0700

    Add Jay Faulkner TC Candidacy
    Change-Id: I7bcf10611b93a3db21b75a9f7e280a4689c2dff3

---
+++ candidates/antelope/TC/jay@jvf.cc
@@ -0,0 +1,39 @@
+OpenStackers,
+
+I would like to submit my candidacy to serve on the TC.
+
+Back in 2010, I was sitting in the rally room at Rackspace Blacksburg when our
+leaders told us that we were going to open source some of our cloud software
+along with Nasa. At the time; I was starstruck. Open source software had given
+me the opportunity to have a career, and now, I was getting to see a new
+project born. It'd be 2014 before I would actually get to work on OpenStack,
+but that moment is seared in my memory as something special.
+
+Twelve years later -- eight of them spent working in and around OpenStack, I
+find myself in a position to serve the project that I looked at with such wide
+eyes early in my career.
+
+One of OpenStack's true strengths has always been diversity of ideas and
+contributions. My main motivation in running for TC is to ensure that we hang
+on to that strength for as long as possible. I believe that having more
+independent voices on the TC is one way we can do that.
+
+What enables me to be an independent voice? My employer, G-Research Open Source
+Software, supports the operation of OpenStack without it being a part of their
+core business. OpenStack is a tool used, just like so many other companies, as
+part of their workflow to get things done. This is important because it
+demonstrates what they're not: a vendor, hosting service, or reseller of
+OpenStack -- but just a company that is built with OpenStack as an
+infrastructure piece. This means I can leave my "upstream hat" on 24/7 -- a
+luxury that is rare in open source.
+
+The last eight years working on OpenStack, and Ironic specifically, have been
+formative to my career, and to me personally. I have made and connected with
+friendships that will hopefully outlast the computers we all provision
+together. The OpenStack community has always been kind to me, and if given the
+opportunity, I'll do everything I can to repay that kindness and ensure the
+community remains welcoming during my tenure on the TC.
+
+Thanks for reading,
+Jay Faulkner
+IRC: JayF
