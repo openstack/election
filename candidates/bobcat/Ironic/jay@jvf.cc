87af17a629b7aaea5104e7bc7e4c739de2af387d
Jay Faulkner <jay@jvf.cc>
2023-02-01 13:39:02 -0800

    Jay Faulkner candidacy for Ironic PTL 2023.2
    Change-Id: If4b598b90e78e0e6e038f44a990b75d8063473d3

---
+++ candidates/bobcat/Ironic/jay@jvf.cc
@@ -0,0 +1,28 @@
+Friends and Colleagues,
+
+I would like to submit my candidacy to serve as Ironic PTL for the 2023.2
+cycle.
+
+The Antelope cycle was a learning experience for me, but we have achieved
+and are still achieving many of our goals. 
+
+Some specific items I'm proud to have championed or accomplished include:
+
+- Unmaintained bugfix branches from the last several years have been
+  cleaned up, making Ironic less of a burden on the commons.
+- Worked cross-project with members of the Nova team, working to make
+  Ironic scale with Nova computes in a way more inline with how Nova
+  expects their services to scale.
+- Performing soft influencer work to try and get Ironic more in the
+  spotlight. These efforts included running semi-weekly streams where
+  I did live OSS work, including at times working through issues with
+  other Ironic contributors. I also have been active on socials doing
+  what I can to share the word about the good stuff we're doing.
+
+Ironic continues to be a large part of the open source ecosystem, and
+I'm excited at the chance to lead for another cycle.
+
+Thanks,
+Jay Faulkner
+
+IRC: JayF
