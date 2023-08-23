af6fffbbdeadfbaafcc010daf8acf485867a4801
Jay Faulkner <jay@jvf.cc>
2023-08-23 08:11:03 -0700

    Jay Faulkner (JayF) PTL Candidacy for Ironic
    Running for Ironic PTL 2024.1
    
    Change-Id: I4f28e03278cd45892e6152fda2e728a546eb25f0

---
+++ candidates/2024.1/Ironic/jay@jvf.cc
@@ -0,0 +1,10 @@
+Friends and Colleagues,
+
+I would like to submit my candidacy to serve as Ironic PTL for the 2024.1
+cycle. If elected, this will be my third cycle as PTL of Ironic. I'm happy
+to continue to serve the community if you will have me.
+
+Thanks,
+Jay Faulkner
+
+IRC: JayF
